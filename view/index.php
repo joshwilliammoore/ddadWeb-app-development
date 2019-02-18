0530bed4c110e6c128da15322dcb55f934c36411
k1409896 <k1409896@kingston.ac.uk>
2019-02-18 19:13:30 +0000

    Index

---
+++ view/index.php
@@ -0,0 +1,15 @@
+<?php
+   ?>
+<!doctype html>
+<html>
+   <head>
+      <link href="../CSS/main.css" rel="stylesheet" type="text/css">
+      <title>Home</title>
+      <div class = main>
+         <h1>Home</h1>
+      </div>
+   </head>
+   <body>
+      
+   </body>
+</html>
