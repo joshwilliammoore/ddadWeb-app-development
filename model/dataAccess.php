7059f3e0cd956812964102bd0f96fee6b3a62697
Joshua Moore <K1720552@kingston.ac.uk>
2019-02-28 14:16:52 +0000

    Changes to dataAccess

--- model/dataAccess.php
+++ model/dataAccess.php
@@ -27,36 +27,12 @@ function getAllVehiclesPrice()
 function getVehiclesByPassengers($vehicle)
 	{
 	global $pdo;
-	$statement = $pdo->prepare("SELECT * FROM vehicles WHERE vehicleModel LIKE '%{$vehicle}%' OR driving_license_required LIKE '%{$vehicle}%' OR id LIKE '%{$vehicle}%' OR number_of_passengers LIKE '%{$vehicle}%' OR price LIKE '%{$vehicle}%' OR date_available LIKE '%{$vehicle}%'");
+	$statement = $pdo->prepare("SELECT * FROM vehicles WHERE driving_license_required LIKE '%{$vehicle}%' OR id LIKE '%{$vehicle}%' OR number_of_passengers LIKE '%{$vehicle}%' OR price LIKE '%{$vehicle}%' OR date_available LIKE '%{$vehicle}%'");
 	$statement->execute([$vehicle]);
 	$results = $statement->fetchAll(PDO::FETCH_CLASS, "vehicle");
 	return $results;
    }
-
-function addNewVehicle($number_of_passengers, $model, $date_available, $price, $license)
-{
-	global $pdo;
-	$statement = $pdo->prepare("INSERT INTO vehicles (number_of_passengers, vehicleModel, date_available, price, driving_license_required) VALUES ($number_of_passengers, '$model', $date_available, $price, '$license')");
-	$statement ->execute([$number_of_passengers, $model, $date_available, $price, $license]);
-}
-
 function users(){
 
 }
-
-function userLogin($username, $password){
-	global $pdo;
-	$statement = $pdo->prepare("SELECT username FROM customer_login LIKE '%{$username}%'");
-	$statement = $pdo->execute([$username, $password]);
-	$results = $statement->fetchAll(PDO::FETCH_CLASS, "customer_login");
-	return results;
-}
-
-function addCustomer($fist_name, $second_name, $address, $email_address, $contact_number)
-{
-	global $pdo;
-	$statement = $pdo->prepare("INSERT INTO customers (customer_lastname, customer_firstname, customer_address, email_address, contact_number) VALUES ($second_name, $first_name, $address, $email_address, $contact_number)");
-	$statement ->execute([$second_name, $first_name, $address, $email_address, $contact_number]);
-}
-
 ?>
