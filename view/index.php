1948ef9f54c8033634fe5ab329a722532e8ecbef
qasim6666 <qali3385@gmail.com>
2019-03-21 12:50:06 +0000

    chnages to index

--- view/index.php
+++ view/index.php
@@ -34,6 +34,7 @@
 ?>
 <!doctype html5>
 <html>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <head>
     <link href="../CSS/main.css" rel="stylesheet" type="text/css">
