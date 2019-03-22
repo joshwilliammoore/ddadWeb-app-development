9d150d8568b605a22a11729123150183bbf1d590
Kumbla <K1710110@kingston.ac.uk>
2019-03-22 15:32:22 +0000

    promotion.php page in controler and model

---
+++ controler/promotions.php
@@ -0,0 +1,44 @@
+<?php
+require_once "../model/vehicle.php";
+require_once "../view/vehiclelist_view.php";
+require_once "../model/dataAccess.php";
+if (!isset($_REQUEST["searchname"]) && !isset($_REQUEST["price"]) && !isset($_REQUEST["vehicleID"]) && !isset($_REQUEST["vehicleModel"]) && !isset($_REQUEST["numberOfPassengers"]) && !isset($_REQUEST["dateAvailable"]) && !isset($_REQUEST["license"])&& !isset($_REQUEST["addToBasket"]))
+{
+    $results = getAllVehicles();
+}
+else if (isset($_REQUEST["searchname"]))
+{
+    $search = $_REQUEST["searchname"];
+    $results = getVehiclesBySearch($search);
+}
+else if (isset($_REQUEST["vehicleID"]))
+{
+    $results = getAllVehiclesByVehicleID();
+}
+else if (isset($_REQUEST["price"]))
+{
+    $results = getAllVehiclesByPrice();
+}
+else if (isset($_REQUEST["vehicleModel"]))
+{
+    $results = getAllVehiclesByVehicleModel();
+}
+else if (isset($_REQUEST["numberOfPassengers"]))
+{
+    $results = getAllVehiclesByNumberOfPassengers();
+}
+else if (isset($_REQUEST["dateAvailable"]))
+{
+    $results = getAllVehiclesByDateAvailable();
+}
+else if (isset($_REQUEST["license"]))
+{
+    $results = getAllVehiclesByLicense();
+}
+else if (isset($_REQUEST["addToBasket"]))
+{
+    $id = $_REQUEST["vehiclesID"];
+    $array = addVehicleToBasket($id);
+    $results = getAllVehicles();
+}
+?>
---
+++ model/promotion.php
@@ -0,0 +1,23 @@
+<?php
+class vehicle
+{
+	private $vehicle_id;
+	private $vehicle_type;
+	private $vehicle_model;
+	private $number_of_passengers;
+	private $price;
+	private $driving_license_required;
+	private $vehicle_date;
+
+	function __get($name)
+	{
+		return $this->$name;
+	}
+
+	function __set($name,$value)
+	{
+		$this->$name = $value;
+	}
+
+}
+?>
